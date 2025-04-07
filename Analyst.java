a1441530a166a51101bd0d1328adb7eeb4407a58
Scedxing <mijiniket249@gmail.com>
2025-04-07 16:22:43 +0800

    deleted file

--- Analyst.java
+++
@@ -1,9 +0,0 @@
-public class Analyst extends Employee {
-
-    public Analyst(String name, double salary, int age) {
-        super(name, salary, age);
-    }
-    public double getAnnualBonus() {
-        return super.salary * .05;
-    }
-}
